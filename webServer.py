2b253bba3260c1861aae589ce8dd996219967eea
Engr. Lorenzo Galante <galantelorenzo97@gmail.com>
2023-02-16 14:07:13 -0500

    Changed reciever buffer to 2048 for experimental purposes

--- webServer.py
+++ webServer.py
@@ -22,7 +22,7 @@ def webServer(port=13331):
     connectionSocket, addr = serverSocket.accept()
     
     try:
-      message = connectionSocket.recv().decode()
+      message = connectionSocket.recv(2048).decode()
       filename = message.split()[1]
       
       #opens the client requested file. 
