3cd86bb4be8f645052ba437f2d8edb2f6d77d74b
Engr. Lorenzo Galante <galantelorenzo97@gmail.com>
2023-03-04 18:15:23 -0500

    Revert "Forgot to commit"
    This reverts commit d382e4a39d77cd301f21269fd21b6a0cadb24a68.

---
+++ webServer.py
@@ -0,0 +1,79 @@
+# import socket module
+from socket import *
+# In order to terminate the program
+import sys
+
+DEBUG = False
+
+def webServer(port=13331):
+  serverSocket = socket(AF_INET, SOCK_STREAM)
+  
+  #Prepare a server socket
+  serverSocket.bind(("", port))
+  
+  #Fill in start
+  serverSocket.listen(1)
+  #Fill in end
+
+  while True:
+    #Establish the connection
+    
+    print('Ready to serve...')
+    connectionSocket, addr = serverSocket.accept()
+    
+    try:
+      message = connectionSocket.recv(1024)
+      filename = message.split()[1]
+      debugprint(message)
+      
+      #opens the client requested file. 
+      #Plenty of guidance online on how to open and read a file in python. How should you read it though if you plan on sending it through a socket?
+      f = open(filename[1:], "rb") #fill in start #fill in end)
+      #fill in end
+      
+      outputdata = b"HTTP/1.1 200 OK\r\n\r\n"
+      connectionSocket.send(outputdata)
+      #Fill in start -This variable can store your headers you want to send for any valid or invalid request. 
+      #Content-Type above is an example on how to send a header as bytes
+      #Fill in end
+
+      #Send an HTTP header line into socket for a valid request. What header should be sent for a response that is ok? 
+      #Note that a complete header must end with a blank line, creating the four-byte sequence "\r\n\r\n" Refer to https://w3.cs.jmu.edu/kirkpams/OpenCSF/Books/csf/html/TCPSockets.html
+      #Fill in start
+      #connectionSocket.sendall(outputdata)
+      #Fill in end
+               
+      
+      #Send the content of the requested file to the client
+      for line in f: #for line in file
+        connectionSocket.send(line)
+      
+      connectionSocket.close() #closing the connection socket
+      
+    except Exception as e:
+      # Send response message for invalid request due to the file not being found (404)
+      #Fill in start
+      outputdata = b"HTTP/1.1 404 NOT FOUND\r\n\r\n"
+      connectionSocket.send(outputdata)
+      #Fill in end
+
+
+      #Close client socket
+      #Fill in start
+      connectionSocket.close()
+      #Fill in end
+
+  #Commenting out the below, as its technically not required and some students have moved it erroneously in the While loop. DO NOT DO THAT OR YOURE GONNA HAVE A BAD TIME.
+  serverSocket.close()
+  sys.exit()  # Terminate the program after sending the corresponding data
+
+def debugprint(stmt):
+  if DEBUG:
+    print(stmt)
+
+def transmit(conn_sock, bytearray):
+  for i in range(0, len(bytearray)):
+    conn_sock.send(bytearray[i].encode())
+
+if __name__ == "__main__":
+  webServer(13331)
